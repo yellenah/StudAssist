dee702f89585e6e3970966cf83690b8ff79260c4
Zerato7 <162809017+Zerato7@users.noreply.github.com>
2024-03-14 05:07:41 +0100

    suspenzija studenata brisanje studenata/moderatora

---
+++ provera_brisanja.js
@@ -0,0 +1,10 @@
+//let username = document.getElementById("usernameDel");
+
+let contactForm = document.getElementById("form");
+
+contactForm.addEventListener("submit", function (e) {
+    
+    e.preventDefault();
+    window.location.assign("brisanje_moderatora_2.html");
+
+});
--- provera_logovanja.js
+++ provera_logovanja.js
@@ -14,44 +14,49 @@ contactForm.addEventListener("submit", function (e) {
     } else
     if (password.value === "" || password.value === null) {
         messagePassword.push("* Popunite polje");
+    } else 
+    if (username.value === "administrator") {
+        if (password.value.length > 2) {
+            e.preventDefault();
+            window.location.assign("administrator.html");
+        } else {
+            messageUsername = [];
+            errorUsername.innerText = "";
+            messagePassword.push("* Neispravna lozinka");
+            errorPassword.innerText = messagePassword;
+        }
+    } else
+    if (username.value === "moderator") {
+        if (password.value.length > 2) {
+            e.preventDefault();
+            window.location.assign("moderator.html");
+        } else {
+            messageUsername = [];
+            errorUsername.innerText = "";
+            messagePassword.push("* Neispravna lozinka");
+            errorPassword.innerText = messagePassword;
+        }
+    } else
+    if (username.value === "student") {
+        if (password.value.length > 2) {
+            e.preventDefault();
+            window.location.assign("student.html");
+        }
+        else {
+            messageUsername = [];
+            errorUsername.innerText = "";
+            messagePassword.push("* Neispravna lozinka");
+            errorPassword.innerText = messagePassword;
+        }
+    } else {
+        messageUsername.push("* Neispravno ime");
+        errorUsername.innerText = messageUsername;
     }
 
     if (messageUsername.length || messagePassword.length > 0) {
         e.preventDefault();
         errorUsername.innerText = messageUsername;
         errorPassword.innerText = messagePassword;
-    } else
-    if (username.value == "administrator") {
-        e.preventDefault();
-        if (password.value.length > 2) {
-            window.location.assign("administrator.html");
-        } else {
-            messagePassword.push("* Neispravna lozinka");
-            errorPassword.innerText = messagePassword;
-        }
-    } else
-    if (username.value == "moderator") {
-        e.preventDefault();
-        if (password.value.length > 2) {
-            window.location.assign("moderator.html");
-        } else {
-            messagePassword.push("* Neispravna lozinka");
-            errorPassword.innerText = messagePassword;
-        }
-    } else
-    if (username.value == "student") {
-        e.preventDefault();
-        if (password.value.length > 2) {
-            window.location.assign("student.html");
-        }
-        else {
-            messagePassword.push("* Neispravna lozinka");
-            errorPassword.innerText = messagePassword;
-        }
-    } else {
-        e.preventDefault();
-        messageUsername.push("* Neispravno ime");
-        errorUsername.innerText = messageUsername;
     }
 
 });
